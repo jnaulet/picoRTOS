677c8cdd7b8f3d2d1280414b372aaace6fa61c99
rickxxxblaine <Joachim Naulet>
2022-04-07 21:20:34 +0200

    demo: added devkit-mpc5748g SMP software

---
+++ demo/devkit-mpc5748g-SMP/picoRTOSConfig.h
@@ -0,0 +1,31 @@
+#ifndef PICORTOSCONFIG_H
+#define PICORTOSCONFIG_H
+
+/* CLOCKS */
+#define CONFIG_SYSCLK_HZ        4000000
+#define CONFIG_TICK_HZ          1000
+
+/* TASKS */
+#define CONFIG_TASK_COUNT       3
+#define TASK_TICK_PRIO          0
+#define TASK_LED0_PRIO          1
+#define TASK_LED1_PRIO          2
+
+/* STACK */
+#define CONFIG_DEFAULT_STACK_COUNT 256
+
+/* ARCH-specific (MPC574XX) */
+#define CONFIG_ARCH_PPC_E200_INTC       0xfc040000
+#define CONFIG_ARCH_PPC_E200_INTC_IACKR 0xfc040020
+#define CONFIG_ARCH_PPC_E200_INTC_EOIR  0xfc040030
+#define CONFIG_ARCH_PPC_E200_TIMER      0xfff84000
+#define CONFIG_ARCH_PPC_E200_TIMER_IRQ  229
+
+/* SMP */
+#define CONFIG_SMP_CORES          2
+#define CONFIG_ARCH_PPC_E200_SMP_LOCK 0xfc03c000
+
+/* DEBUG */
+#define CONFIG_CHECK_STACK_INTEGRITY
+
+#endif
