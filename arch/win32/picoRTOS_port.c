20dcbfd523adfa5077ce9851454855f743cea150
rickxxxblaine <Joachim Naulet>
2022-04-10 10:38:07 +0200

    win32: picoRTOS port to win32 threads architecture

---
+++ arch/win32/picoRTOS_port.c
@@ -0,0 +1,85 @@
+#include "picoRTOS.h"
+
+#include <windows.h>
+#include <winbase.h>
+#include <stdbool.h>
+
+#if CONFIG_TICK_HZ > 50
+# pragma message("WARNING: Win32 max tick is typically 50hz")
+#endif
+
+/* local data structure */
+struct thread {
+    void *wthread;
+};
+
+static bool suspended = false;
+static struct thread *current_thread;
+
+static void arch_TICK(void)
+{
+    struct thread *t = current_thread;
+
+    current_thread = (struct thread*)picoRTOS_tick((picoRTOS_stack_t*)t);
+    ResumeThread(current_thread->wthread);
+    SuspendThread(t->wthread);
+}
+
+/* FUNCTIONS TO IMPLEMENT */
+
+void arch_init(void)
+{
+}
+
+void arch_suspend(void)
+{
+    suspended = true;
+}
+
+void arch_resume(void)
+{
+    suspended = false;
+}
+
+picoRTOS_stack_t *arch_prepare_stack(struct picoRTOS_task *task)
+{
+    struct thread *t = (struct thread*)task->stack;
+
+    t->wthread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)task->fn, task->priv,
+                              CREATE_SUSPENDED | STACK_SIZE_PARAM_IS_A_RESERVATION, NULL);
+
+    return (picoRTOS_stack_t*)t;
+}
+
+void arch_start_first_task(picoRTOS_stack_t *sp)
+{
+    current_thread = (struct thread*)sp;
+    ResumeThread(current_thread->wthread);
+
+    /* generate non-RT tick */
+    for (;;) {
+        Sleep(1000 / CONFIG_TICK_HZ);
+
+        if (!suspended)
+            arch_TICK();
+    }
+}
+
+void arch_yield(void)
+{
+    struct thread *t = current_thread;
+
+    current_thread = (struct thread*)picoRTOS_switch_context((picoRTOS_stack_t*)t);
+    ResumeThread(current_thread->wthread);
+
+    /* wait */
+    SuspendThread(t->wthread);
+}
+
+void arch_idle(void *null)
+{
+    arch_assert(null == NULL);
+
+    for (;;)
+        Sleep(1000);
+}
---
+++ arch/win32/picoRTOS_types.h
@@ -0,0 +1,16 @@
+#ifndef PICORTOS_TYPES_H
+#define PICORTOS_TYPES_H
+
+#include <assert.h>
+
+typedef unsigned long picoRTOS_stack_t;
+typedef unsigned long picoRTOS_size_t;
+typedef unsigned long picoRTOS_priority_t;
+typedef unsigned long picoRTOS_tick_t;
+
+#define ARCH_INITIAL_STACK_COUNT 10
+#define ARCH_MIN_STACK_COUNT ARCH_INITIAL_STACK_COUNT
+
+#define arch_assert(x) if (!(x)) assert(x)
+
+#endif
