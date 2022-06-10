e46b7ab31f5270902a5cda71ce43cb5311843db2
rickxxxblaine <Joachim Naulet>
2022-06-10 22:54:16 +0200

    cm3: basic interrupt support

--- arch/arm/cm3/picoRTOS_types.h
+++ arch/arm/cm3/picoRTOS_types.h
@@ -5,6 +5,7 @@ typedef unsigned long picoRTOS_stack_t;
 typedef unsigned long picoRTOS_tick_t;
 typedef unsigned long picoRTOS_priority_t;
 typedef unsigned long picoRTOS_atomic_t;
+typedef unsigned long picoRTOS_irq_t;
 
 #define ARCH_INITIAL_STACK_COUNT 16
 #define ARCH_MIN_STACK_COUNT (ARCH_INITIAL_STACK_COUNT + 8) /* must be 8-bytes "aligned" */
